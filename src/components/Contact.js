1d177dba7a4b73a5db6a0c8ac54f1c588c12137e
Cyberbot777 <cyberbot7777777@gmail.com>
2025-05-24 22:13:28 -0700

    Updated header

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,16 @@
 // src/components/Contact.js
 import React from 'react';
-import { Container, Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './Contact.css';
 
 const Contact = () => (
-  <Container className="text-center my-4">
+  <div className="text-center my-4">
     <h2>Contact</h2>
     <div>
       <Button className="custom-btn mx-2" href="https://github.com/Cyberbot777">GitHub</Button>
       <Button className="custom-btn mx-2" href="https://www.linkedin.com/in/richard-h-169b08364/">LinkedIn</Button>
     </div>
-  </Container>
+  </div>
 );
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,25 @@
 // src/components/Header.js
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import profilePic from '../assets/profile-pic.jpg';
 import './Header.css';
 
 const Header = () => (
   <Container className="py-4 mb-4 mt-4 header-container">
-    <Row className="align-items-center">
-      <Col xs={4} className="text-center pic-container">
-        <img
-          src={profilePic}
-          alt="Richard Hall"
-          className="profile-pic"
-          style={{ width: '150px', height: '170px' }}
-        />
-      </Col>
-      <Col xs={8}>
+    <div className="header-content">
+      <img
+        src={profilePic}
+        alt="Richard Hall"
+        className="profile-pic"
+        style={{ width: '150px', height: '170px' }}
+      />
+      <div className="header-text-centered">
         <h1 style={{ fontWeight: 'bold', fontSize: '3rem' }}>Richard Hall</h1>
-        <div className="text-left" style={{ paddingLeft: '45px' }}>
+        <div className="text-center">
           <h2 style={{ fontSize: '1.5rem' }}>Full Stack Developer</h2>
         </div>
-      </Col>
-    </Row>
+      </div>
+    </div>
   </Container>
 );
 

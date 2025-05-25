6f47a47078bfea509be91503b6ff0a84054a3db8
Cyberbot777 <cyberbot7777777@gmail.com>
2025-05-24 21:33:38 -0700

    Updated background photo. Cleaned up container layering

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,27 @@
 // src/components/Header.js
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import profilePic from '../assets/profile-pic.jpg';
 import './Header.css';
 
 const Header = () => (
   <Container className="py-4 mb-4 mt-4 header-container">
-    <div className="header-content">
-      <img
-        src={profilePic}
-        alt="Richard Hall"
-        className="profile-pic"
-        style={{ width: '150px', height: '170px' }}
-      />
-      <div className="header-text">
-        <h1>Richard Hall</h1>
-        <h2>Full Stack Developer</h2>
-      </div>
-    </div>
+    <Row className="align-items-center">
+      <Col xs={4} className="text-center pic-container">
+        <img
+          src={profilePic}
+          alt="Richard Hall"
+          className="profile-pic"
+          style={{ width: '150px', height: '170px' }}
+        />
+      </Col>
+      <Col xs={8}>
+        <h1 style={{ fontWeight: 'bold', fontSize: '3rem' }}>Richard Hall</h1>
+        <div className="text-left" style={{ paddingLeft: '45px' }}>
+          <h2 style={{ fontSize: '1.5rem' }}>Full Stack Developer</h2>
+        </div>
+      </Col>
+    </Row>
   </Container>
 );
 

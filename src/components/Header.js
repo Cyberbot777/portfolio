fe947f675e988abd4f146842ba3aa292da667a88
Cyberbot777 <cyberbot7777777@gmail.com>
2025-05-24 23:42:00 -0700

    Update container and card backgrounds to rgba(0, 0, 0, 0.7)

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Container } from 'react-bootstrap';
 import './Header.css';
 
 const Header = () => (
-  <Container className="py-4 mb-4 mt-4 header-container">
+  <Container className="py-4 mb-4 mt-4 header-container d-flex justify-content-center align-items-center">
     <div className="header-content">
       <div className="header-text-centered">
         <h1 style={{ fontWeight: 'bold', fontSize: '3rem' }}>Richard Hall</h1>

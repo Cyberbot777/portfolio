376137a58ad794888891d106101c9c7a4531f54d
Cyberbot777 <cyberbot7777777@gmail.com>
2025-05-26 16:03:16 -0700

    Adjust styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    optimizeCss: true, // Inline critical CSS to reduce FOUC
+  },
+};
 
 export default nextConfig;
